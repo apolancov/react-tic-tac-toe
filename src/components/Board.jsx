fcb636155b7e04a427970f9878ae89d775c06e26
Aris Valdez <samus45@hotmail.es>
2024-06-29 21:18:23 -0400

    first commit

---
+++ src/components/Board.jsx
@@ -0,0 +1,18 @@
+import { Square } from "./Square";
+
+export const Board = ({ board, updateBoard }) =>
+    <section className='game'>
+        {
+            board.map((_, index) => {
+                return (
+                    <Square
+                        key={index}
+                        index={index}
+                        updateBoard={updateBoard}
+                    >
+                        {board[index]}
+                    </Square>
+                )
+            })
+        }
+    </section>
